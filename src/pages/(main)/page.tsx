d44a98ebe9d4adb3ceb72923ac21fb7b80289844
okanay <okanay@hotmail.com>
2024-01-24 18:16:05 +0300

    hero 3

--- src/pages/(main)/page.tsx
+++ src/pages/(main)/page.tsx
@@ -6,7 +6,7 @@ export const MainPage = () => {
   return (
     <main>
       <div className={"relative h-svh w-full"}>
-        <div className="relative mx-auto h-full min-h-[56rem] w-full max-w-7xl px-4">
+        <div className="relative mx-auto h-full w-full max-w-7xl px-4 sm:min-h-[56rem]">
           <Browser />
           <GettingStart />
         </div>
