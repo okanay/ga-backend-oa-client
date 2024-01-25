5d29cb09caebee54962ffe15e9d40259374872e3
okanay <okanay@hotmail.com>
2024-01-25 15:18:47 +0300

    hero 11

--- src/components/main/index/hero-section/arrow-animation/grid-cols/index.tsx
+++ src/components/main/index/hero-section/arrow-animation/grid-cols/index.tsx
@@ -2,10 +2,10 @@ import { useMediaQuery } from "@mantine/hooks";
 
 export const GridCols = ({ children }: { children: React.ReactNode }) => {
   const matches = useMediaQuery("(max-width: 640px)");
-  const length = matches ? 5 : 10;
+  const length = matches ? 4 : 10;
 
   return (
-    <div className="grid h-full w-full grid-cols-5 gap-4 sm:grid-cols-10">
+    <div className="grid h-full w-full grid-cols-4 gap-4 sm:grid-cols-10">
       {Array.from({ length }).map((_, columnIndex) => (
         <div key={`column-${columnIndex}`} className="h-full w-full">
           {children}
--- src/components/main/index/hero-section/arrow-animation/grid-rows/index.tsx
+++ src/components/main/index/hero-section/arrow-animation/grid-rows/index.tsx
@@ -2,10 +2,10 @@ import { useMediaQuery } from "@mantine/hooks";
 
 export const GridRows = ({ children }: { children: React.ReactNode }) => {
   const matches = useMediaQuery("(max-width: 640px)");
-  const length = matches ? 7 : 10;
+  const length = matches ? 5 : 10;
 
   return (
-    <div className={"grid h-full grid-rows-7 gap-4 sm:grid-rows-10"}>
+    <div className={"grid h-full grid-rows-5 gap-4 sm:grid-rows-10"}>
       {Array.from({ length }).map((_, rowIndex) => (
         <div key={`key={\`row-${rowIndex}-up\`}`} className={""}>
           {children}
