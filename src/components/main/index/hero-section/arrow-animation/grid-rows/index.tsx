a684f0fb8d12629ffca55b02c9591ff051c15ba6
okanay <okanay@hotmail.com>
2024-01-25 15:14:02 +0300

    hero 10

--- src/components/main/index/hero-section/arrow-animation/grid-rows/index.tsx
+++ src/components/main/index/hero-section/arrow-animation/grid-rows/index.tsx
@@ -1,7 +1,12 @@
+import { useMediaQuery } from "@mantine/hooks";
+
 export const GridRows = ({ children }: { children: React.ReactNode }) => {
+  const matches = useMediaQuery("(max-width: 640px)");
+  const length = matches ? 7 : 10;
+
   return (
-    <div className={"grid h-full grid-rows-10 gap-4"}>
-      {Array.from({ length: 10 }).map((_, rowIndex) => (
+    <div className={"grid h-full grid-rows-7 gap-4 sm:grid-rows-10"}>
+      {Array.from({ length }).map((_, rowIndex) => (
         <div key={`key={\`row-${rowIndex}-up\`}`} className={""}>
           {children}
         </div>
