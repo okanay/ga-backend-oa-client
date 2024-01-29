6d8b4a095558341b1e876ac1f52e6bb1adf56b30
okanay <okanay@hotmail.com>
2024-01-29 18:55:41 +0300

    features - 20

--- src/components/main/index/index.tsx
+++ src/components/main/index/index.tsx
@@ -1,8 +1,8 @@
 import { HeroSection } from "@/components/main/index/hero-section";
-import { PriceSection } from "@/components/main/index/price-section";
-import { FeatureSection } from "@/components/main/index/feature-section";
-import { FaqSection } from "@/components/main/index/faq-section";
-import { ContactForm } from "@/components/main/index/contact-form";
+// import { PriceSection } from "@/components/main/index/price-section";
+// import { FeatureSection } from "@/components/main/index/feature-section";
+// import { FaqSection } from "@/components/main/index/faq-section";
+// import { ContactForm } from "@/components/main/index/contact-form";
 
 export const IndexSections = () => {
   return (
