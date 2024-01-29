254b57a1e4b024a9d56abb59d235307ec90f6e52
okanay <okanay@hotmail.com>
2024-01-29 17:07:50 +0300

    features - 8

--- src/components/main/index/feature-section/index.tsx
+++ src/components/main/index/feature-section/index.tsx
@@ -5,7 +5,7 @@ import { FeatureItem } from "@/components/main/index/feature-section/feature-ite
 export const FeatureSection = () => {
   return (
     <section className={"bg-ebony-950 "}>
-      <div className="relative mx-auto max-w-[64rem] space-y-12 px-8 pb-24 pt-12">
+      <div className="relative mx-auto max-w-[64rem] space-y-12 px-8 py-12">
         <FeatureHeader />
         <div className={"flex w-full flex-col text-ebony-950"}>
           {featuresData.map((feature, index) => (
