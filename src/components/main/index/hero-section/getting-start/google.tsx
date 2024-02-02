96bb4f5383f889cdcb9e0016651b146885b008aa
okanay <okanay@hotmail.com>
2024-02-03 02:52:51 +0300

    font index html 8

--- src/components/main/index/hero-section/getting-start/google.tsx
+++ src/components/main/index/hero-section/getting-start/google.tsx
@@ -37,7 +37,7 @@ export const Google = () => {
           <span>{letter}</span>
           <m.div
             initial={{ y: 0 }}
-            animate={{ y: "-105%", opacity: 0.75 }}
+            animate={{ y: "-108%", opacity: 0.75 }}
             transition={{
               duration: 0.75,
               delay: 0.25 + index * 0.05,
