86e1e0634cda5c71714fcc489a6fe6ec40c85813
okanay <okanay@hotmail.com>
2024-02-03 02:53:55 +0300

    font index html 9

--- src/components/main/index/hero-section/getting-start/index.tsx
+++ src/components/main/index/hero-section/getting-start/index.tsx
@@ -5,7 +5,7 @@ import { Title } from "./title.tsx";
 
 export const GettingStart = () => {
   return (
-    <div className="flex w-full max-w-5xl flex-col items-center justify-center gap-8 base-phone:gap-12">
+    <div className="relative z-30 flex w-full max-w-5xl flex-col items-center justify-center gap-8 base-phone:gap-12">
       <Title />
       <Description />
       <div className={"flex items-center justify-center gap-2 sm:gap-4"}>
