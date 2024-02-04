5fa63add0217b183d3d10c33d0b35a3a57e034da
okanay <okanay@hotmail.com>
2024-02-04 21:44:59 +0300

    test

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import { DevModeTailwindIndicator } from "@/components/ui/tailwind-indicator.tsx";
 
 export const AppLayout = () => {
+  //   Test Modify.
+
   return (
     <div className={"font-plex-sans"}>
       <Outlet />
