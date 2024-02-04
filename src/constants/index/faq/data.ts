49fe16c2fd69f99940be25620305d661128334ce
okanay <okanay@hotmail.com>
2024-02-04 17:40:52 +0300

    auth 06

--- src/constants/index/faq/data.ts
+++ src/constants/index/faq/data.ts
@@ -2,36 +2,26 @@ export const FaqData = [
   {
     question: "How do I get my website on Google?",
     answer:
-      "Submit your site's URL to Google Search Console and let their web crawlers do the rest. ***Site İsmi*** can automate this process for you.",
+      "Submit your site's URL to Google Search Console and let their web crawlers do the rest. Search Pusher can automate this process for you.",
   },
   {
     question: "How do I get Google to crawl my site?",
     answer:
-      "Submit a sitemap of your site to Google Search Console. This helps their crawlers find and index your pages. ***Site İsmi*** can check for sitemap errors.",
+      "Submit a sitemap of your site to Google Search Console. This helps their crawlers find and index your pages. Search Pusher can check for sitemap errors.",
   },
   {
     question: "My site was crawled but not indexed. What's wrong?",
     answer:
-      "This could be due to problems with site structure, content quality, or technical issues. ***Site İsmi*** can help you resubmit pages in bulk.",
+      "This could be due to problems with site structure, content quality, or technical issues. Search Pusher can help you resubmit pages in bulk.",
   },
   {
     question: "What does 'discovered currently not indexed' mean?",
     answer:
-      "Google found your site but hasn't added it to their index yet. ***Site İsmi*** can help you address potential issues and resubmit pages.",
+      "Google found your site but hasn't added it to their index yet. Search Pusher can help you address potential issues and resubmit pages.",
   },
   {
     question: "What is search engine indexing?",
     answer:
-      "It's the process of adding web pages to a search engine's database so they can appear in search results. ***Site İsmi*** can help speed up indexing.",
+      "It's the process of adding web pages to a search engine's database so they can appear in search results. Search Pusher can help speed up indexing.",
   },
-  // {
-  //   question: "How do I remove my site from Google?",
-  //   answer:
-  //     "Request a removal through Google Search Console. ***Site İsmi*** can assist with this process.",
-  // },
-  // {
-  //   question: "My page was crawled but not indexed. What can I do?",
-  //   answer:
-  //     "Use ***Site İsmi*** to resubmit your pages and increase the chance of indexing.",
-  // },
 ] as const;
